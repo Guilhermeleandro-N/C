75eab09e7a16da29f06247576bb8503f767d679e
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-02-06 15:59:57 -0300

    Update string sem ponteiros.c
    fgets

--- ponteiro/string sem ponteiros.c
+++ ponteiro/string sem ponteiros.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 
@@ -6,15 +5,14 @@ int main()
 {
     int comprimento;
     char frase[100], novafrase[100];
-    scanf("%s",frase);
+    fgets(frase,100,stdin);
     comprimento=strlen(frase);
     for(int c=0;c<comprimento;c++){
         if (!(frase[c]==97 || frase[c]==101 || frase[c]==105 || frase[c]==111 || frase[c]==117 || frase[c]==65 || frase[c]==69 ||frase[c]==73 || frase[c]==79 || frase[c]==85)){
           novafrase[c]=frase[c];
         }else{
-            novafrase[c]=" ";
+            novafrase[c]=32;
         }
     }
     printf("Nova frase: %s",novafrase);
 }
-
